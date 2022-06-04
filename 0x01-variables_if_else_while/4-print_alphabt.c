cf4031f1ad93c45a7326bae911d4b427cbc61c14
Elijah Sinkamba <scengj@gmail.com>
2022-06-03 18:56:07 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - print lowercase alphabet
+ * Return: 0
+ */
+
+int main(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		if ((c != 'q') && (c != e))
+		{
+			putchar(c);
+		}
+		c++;
+	}
+
+	putchar(c);
+
+	return (0);
+}
