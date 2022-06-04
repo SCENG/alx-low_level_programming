e2ed6f8d3107c78c50426de2bb6296d287bfd81f
Elijah Sinkamba <scengj@gmail.com>
2022-06-03 19:21:24 -0700

    print numbers using putchar

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - print numbers less 10 using putchar
+ * Return: 0
+ */
+
+int main(void)
+{
+	int num = '0';
+
+	while (n <= '9')
+	{
+		putchar(num);
+		num++;
+	}
+	putchar('\n');
+
+	return (0);
+}
