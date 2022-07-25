830fd853909e90bb06727597ed61b9c06208c577
Elijah Sinkamba <chisanga110@live.com>
2022-07-25 23:47:24 +0200

    elf header

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,6 +1,17 @@
 #ifndef _MAIN_H
 #define _MAIN_H
 
+void check_elf(unsigned char *e_ident);
+void print_magic(unsigned char *e_ident);
+void print_class(unsigned char *e_ident);
+void print_data(unsigned char *e_ident);
+void print_version(unsigned char *e_ident);
+void print_abi(unsigned char *e_ident);
+void print_osabi(unsigned char *e_ident);
+void print_type(unsigned int e_type, unsigned char *e_ident);
+void print_entry(unsigned long int e_entry, unsigned char *e_ident);
+void close_elf(int elf);
+
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/stat.h>
